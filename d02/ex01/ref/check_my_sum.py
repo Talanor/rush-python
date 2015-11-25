fec02772ab003fbb482af7a59d1b8fdfe479562e
Ventre Louis <oxydros@gmail.com>
2015-11-25 13:41:21 +0100

    Fix pep8 for d02/ex01, improve script

--- d02/ex01/ref/check_my_sum.py
+++ d02/ex01/ref/check_my_sum.py
@@ -1,27 +1,30 @@
 #!/bin/python3
 
 import sys
-import json
 import argparse
 import requests
 import hashlib
 import base64
 import urllib.parse
 
+
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Output the content of a file in base 64")
+    parser = argparse.ArgumentParser(description="Output the content of a "
+                                     "file in base 64")
     parser.add_argument("url", help="Valid url")
-    parser.add_argument("hash_md5", help="The md5 hash of the file you want to get.")
-    args = parser.parse_args()
-    return args
+    parser.add_argument("hash_md5", help="The md5 hash of the file you want to"
+                        "get.")
+    return parser.parse_args()
+
 
 def get_page_content(session, url):
     try:
         response = session.get(url)
-    except Exception:
-        print('ERROR: Unable to access the given URL.')
+    except requests.ConnectionError:
+        print('ERROR: Unable to access the given URL.', file=sys.stderr)
         sys.exit(1)
-    return response.content.decode()
+    return response.json()
+
 
 def get_correct_file_content(session, url, hash_md5, file_infos):
     for file_inf in file_infos:
@@ -34,17 +37,20 @@ def get_correct_file_content(session, url, hash_md5, file_infos):
             return resp.content
     return None
 
+
 def main():
     args = parse_arguments()
     session = requests.session()
-    page_content = get_page_content(session, args.url)
-    file_infos = json.loads(page_content)
-    file_content = get_correct_file_content(session, args.url, args.hash_md5, file_infos)
+    file_infos = get_page_content(session, args.url)
+    file_content = get_correct_file_content(session, args.url, args.hash_md5,
+                                            file_infos)
     if not file_content:
-        print('ERROR: Unable to find the file corresponding to the given md5sum ("%s").'%(args.hash_md5))
+        print('ERROR: Unable to find the file corresponding to the given '
+              'md5sum ("%s").' % (args.hash_md5), file=sys.stderr)
     else:
         print(base64.b64encode(file_content).decode())
     session.close()
-    
+
+
 if __name__ == "__main__":
     main()
