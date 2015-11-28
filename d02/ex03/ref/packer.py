15c5d7a82ec7a0e0305a91cb24c03a187000f415
SiN <amaury.ravanel@epitech.eu>
2015-11-28 12:43:43 +0100

    INFO: maj of exercise 3 of day 2 with som raise

--- d02/ex03/ref/packer.py
+++ d02/ex03/ref/packer.py
@@ -10,40 +10,61 @@ class packer(object):
     @staticmethod
     def networkPackData(*args, **kwargs):
         _format = '!'
+        data = []
         for item in args:
             if type(item) is int:
-                format += 'I'
+                _format += 'I'
+                data.append(item)
             elif type(item) is str:
-                format += '%ds' % (len(item))
+                _format += '%ds' % (len(item))
+                data.append(bytes(item, 'ascii'))
             elif type(item) is float:
-                format += 'f'
-        return struct.pack(_format, *args)
+                _format += 'f'
+                data.append(bytes(item, 'ascii'))
+            else:
+                raise NotImplementedError
+        return struct.pack(_format, *data)
 
     @staticmethod
     def bigEndianPackData(*args, **kwargs):
         _format = '>'
+        data = []
         for item in args:
             if type(item) is int:
-                format += 'I'
+                _format += 'I'
+                data.append(item)
             elif type(item) is str:
-                format += '%ds' % (len(item))
+                _format += '%ds' % (len(item))
+                data.append(bytes(item, 'ascii'))
             elif type(item) is float:
-                format += 'f'
-        return struct.pack(_format, *args)
+                _format += 'f'
+                data.append(bytes(item, 'ascii'))
+            else:
+                raise NotImplementedError
+        return struct.pack(_format, *data)
 
 
     @staticmethod
     def littleEndian(*args, **kwargs):
         _format = '<'
+        data = []
         for item in args:
             if type(item) is int:
-                format += 'I'
+                _format += 'I'
+                data.append(item)
             elif type(item) is str:
-                format += '%ds' % (len(item))
+                _format += '%ds' % (len(item))
+                data.append(bytes(item, 'ascii'))
             elif type(item) is float:
-                format += 'f'
-        return struct.pack(_format, *args)
+                _format += 'f'
+                data.append(bytes(item, 'ascii'))
+            else:
+                raise NotImplementedError
+        return struct.pack(_format, *data)
 
     @staticmethod
     def unpackAllDatas(form, data):
         return struct.unpack(form, data)
+
+if __name__ == '__main__':
+    print (packer.networkPackData(8,9,'koala'))
